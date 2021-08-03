a252b6da36fd69954efe86945fa2372baf6d1a90
Lehmaning <60032845+Lehmaning@users.noreply.github.com>
2021-08-03 23:51:47 +0800

    使用紧凑样式 增加单项输出的功能

--- BiliGet.php
+++ BiliGet.php
@@ -16,13 +16,15 @@ class BiliGet {
 	* @see https://www.runoob.com/php/php-ref-curl.html#div-comment-36119
 	*/
 	public static function getUrl($url) {
+		# header( 'Referrer-Policy: no-referrer' ); // 在客户端收到 403 时启用
 		$headerArray = array("Content-type:application/json;","Accept:application/json");
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_REFERER, "www.bilibili.com");
+		if(!empty($aid))curl_setopt($ch, CURLOPT_REFERER, "https://www.bilibili.com/av$aid");
+		else if(!empty($bid))curl_setopt($ch, CURLOPT_REFERER, "https://www.bilibili.com/BV$bvid");
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $headerArray);
 		$output = curl_exec($ch);
 		curl_close($ch);
@@ -32,7 +34,7 @@ class BiliGet {
 
 	public static function url2aid($url) {
 		$id=false;
-		$aid_pattern = "#(https?:\/\/bilibili\.com\/video\/)?av(\d+)(\?p=\d{3})?#";
+		$aid_pattern = "#(https?:\/\/w{3}?\.?bilibili\.com\/video\/)?av(\d+)(\?p=\d{1,3})?#";
 		if(preg_match($aid_pattern, $url, $preg)) {
 			$id = $preg[2];
 			//$id = preg_replace(".*av(\d+).*", "\1", $url);
@@ -41,50 +43,82 @@ class BiliGet {
 	}
 	public static function url2bvid($url) {
 		$id=false;
-		$bvid_pattern = "#(https?:\/\/bilibili\.com\/video\/)?BV([a-zA-Z0-9]+)(\?p=\d{3})?#";
+		$bvid_pattern = "#(https?:\/\/w{3}?\.?bilibili\.com\/video\/)?BV([a-zA-Z0-9]+)(\?p=\d{1,3})?#";
 		if(preg_match($bvid_pattern, $url, $preg)) {
 			$id = $preg[2];
 			//$id = preg_replace(".*BV([a-zA-Z0-9]+).*", "\1", $url);
 		}
 		return $id;
 	}
-	public static function handleDesc($text){
+	public static function handleDesc($text) {
+		$pattern = "##";
 		return 0;	//@todo 简介文字的处理
 	}
 	public static function getInfo($input, $argv, $parser) {
 		$parser->getOutput()->addModules('ext.biliget.list');
-		if( !empty( self::url2aid($input) ) ) {
+		if ( !empty( self::url2aid($input) ) ) {
 			$aid = self::url2aid($input);
+			$id = "av$aid";
 			$data = self::getUrl("https://api.bilibili.com/x/web-interface/view?aid=$aid");
 			$link = "https://bilibili.com/video/av$aid";
 		}
-		if( !empty( self::url2bvid($input) ) ) {
+		if ( !empty( self::url2bvid($input) ) ) {
 			$bvid = self::url2bvid($input);
+			$id = "BV$bvid";
 			$data = self::getUrl("https://api.bilibili.com/x/web-interface/view?bvid=$bvid");
 			$link = "https://bilibili.com/video/BV$bvid";
 		}
-			$code = $data['code'];
-			if ($code != -412){
-				$cover = $data['data']['pic'];
-				$uploader = $data['data']['owner']['name'];
-				$desc = str_replace("\n", "<br />", $data['data']['desc']);
-				$title = $data['data']['title'];
-				$pubdate = date("Y-m-d H:i", $data['data']['pubdate']);
-				$duration = gmstrftime("%M:%S", $data['data']['pages'][0]['duration']);
-				$code =$data['code'];
+		$code = $data['code'];
+		if ($code==0) {
+			$cover = $data['data']['pic'];
+			$uploader = $data['data']['owner']['name'];
+			$desc = str_replace("\n", "<br />", $data['data']['desc']);
+			$title = $data['data']['title'];
+			$pubdate = date("Y-m-d H:i", $data['data']['pubdate']);
+			$duration = gmstrftime("%M:%S", $data['data']['pages'][0]['duration']);
+			if ( !empty($argv['type']) && $argv['type']=='raw' ) {
+				return "$cover|$title|$uploader|$pubdate|$duration|$desc";
+			}
+			else if ( !empty($argv['type'] ) ) {
+				$type = array(
+					'封面' => $cover,
+					'UP主'=> $uploader, 
+					'简介' => $desc,
+					'标题' => $title,
+					'发布日期' => $pubdate,
+					'时长' => $duration
+					);
+				$name = $argv['type'];
+				return $type[trim($name)];
+			}
+			else {
 				return "
 				<div class=\"bili-info-card\">
-					<div class=\"bili-info\" style=\"overflow: hidden\">
+					<div class=\"bili-info\" style=\"width: 80%; overflow: hidden\">
 						<div class=\"bili-info-cover\"><img src=\"$cover\" width=\"160px\" /></div>
 						<p class=\"bili-info-duration\">$duration</p>
 					</div>
 					<div class=\"bili-info\">
-						<p class=\"bili-info-title\" style=\"margin-bottom: 0;\"><a href=\"$link\">$title</a></p>
-						<p class=\"bili-info-date\"	style=\"margin-top: 0;\">$pubdate</p>
-						<p class=\"bili-info-uploader\">UP主：$uploader</p>
+						<p class=\"bili-info-title\" style=\"margin-bottom: 0\"><a href=\"$link\" title=\"$id\">$title</a></p>
+						<p class=\"bili-info-date\"	style=\"margin-top: 0; margin-bottom: 0\">$pubdate</p>
+						<p class=\"bili-info-uploader\" style=\"margin-top: 0;\">UP主：$uploader</p>
 						<p class=\"bili-info-desc\">$desc</p>
 					</div>
-				</div>";}
-			//@todo 简介折叠、链接和作品号自动变蓝
+				</div>";
+					}
+				}
+		else {
+			$message = $data['message'];
+			return "
+				<div class=\"bili-info-card\">
+					<div class=\"bili-info\" style=\"width: 80%\">
+						<p>错误码：$code</p>
+					</div>
+					<div class=\"bili-info\">
+						<p class=\"bili-info-title\">$message</p>
+					</div>
+				</div>";
+		}
+	//@todo 简介折叠、链接和作品号自动变蓝
 	}	
 }
